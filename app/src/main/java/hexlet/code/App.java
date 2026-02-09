e67787d17f66a7dedbe197f6b9aa2ee96a1b78ee
Anani Oganesian <oganesian.anani@gmail.com>
2026-02-09 22:19:33 +0300

    added a makefile, imported scanner, modified code

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,13 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
     }
 }
