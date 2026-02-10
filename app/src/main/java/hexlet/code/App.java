313e84134371f5fe3c430f6334f34a629d0dae4c
Anani Oganesian <oganesian.anani@gmail.com>
2026-02-10 23:03:10 +0300

    deleted Scanner, added link to CLi class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,10 @@
 package hexlet.code;
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
+        // Выводим приветствие
         System.out.println("Welcome to the Brain Games!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("May I have your name? ");
-        String name = scanner.nextLine();
-        System.out.println("Hello, " + name + "!");
-        scanner.close();
+        Cli.getName();
     }
 }
