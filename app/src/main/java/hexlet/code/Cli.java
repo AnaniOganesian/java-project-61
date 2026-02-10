2bef70e325640a7001c88c6dd0315fe1891cf26c
Anani Oganesian <oganesian.anani@gmail.com>
2026-02-10 23:03:53 +0300

    added new class with Scanner

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,19 @@
+package hexlet.code;
+import java.util.Scanner; // испортировали сканнер для ввода текста
+// согласно заданию
+
+public class Cli {
+
+    public static void getName() {
+    // Создаем объект класса сканнер, который считывает введеный текст
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("May I have your name? ");
+    // Ввод имени
+    String name = scanner.nextLine();
+
+    System.out.println("Hello, " + name + "!");
+    scanner.close();
+
+    }
+}
