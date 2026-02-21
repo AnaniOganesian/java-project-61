1c2f2992e3870cee98ab31de0a25b5b0c3ddf259
Anani Oganesian <oganesian.anani@gmail.com>
2026-02-21 19:43:55 +0300

    hexlet check working unproperly?

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -25,7 +25,7 @@ public class Engine {
     }
 
     public void greeting() {
-        System.out.println("Welcome to the Brain Games!");
+        //System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         // Ввод имени
         this.playerName = scanner.nextLine().trim();
