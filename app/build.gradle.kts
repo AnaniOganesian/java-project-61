59a1336e5f14a02804be2abc911e79e60676b453
Anani Oganesian <oganesian.anani@gmail.com>
2026-02-25 22:42:47 +0300

    deleted tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,14 +22,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.apache.commons:commons-lang3:3.20.0")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
 
 sonar {
